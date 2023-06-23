d90751ff392a3b18e308abf8c6eda09859b4432a
poldev16 <pol.dev16@gmail.com>
2023-06-23 21:22:30 +0530

    test server

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyparser = require("body-parser");
 const app = express();
 app.use(express.json());
 require("dotenv").config();
-require("./utils/db_connection.js");
+// require("./utils/db_connection.js");
 var cors = require('cors')
 
 app.use(cors())
@@ -17,7 +17,7 @@ app.get("/home", (req, res) => {
     res.send("welcome to home page mandal")
 })
 
-require("./route/roter.js")(app);
+// require("./route/roter.js")(app);
 
 app.listen(process.env.PORT, () => {
     console.log(`server start on localhost:${process.env.PORT}`)
